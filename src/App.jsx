8de7a46032bfd2361ee234a314b59f68e26e6788
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2023-11-16 21:51:40 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,18 @@ function App() {
   useEffect(() => {
     window.addEventListener("resize", nowSizeWith);
 
-    // Clean up 🤌🏼
+    // Clean up
     return () => {
       window.removeEventListener("resize", nowSizeWith);
     };
   });
 
   return (
-    <div className=" w-full h-screen bg-amber-500 flex flex-col items-center justify-center">
-      <h1 className="text-[40px] font-bold">
+    <div className=" w-full h-screen bg-gray-300 flex flex-col items-center justify-center">
+      <h1 className="text-[40px] font-bold text-gray-600 selection:text-amber-600">
         The actual size of the window is
       </h1>
-      <p className="text-[90px] font-bold mt-4 text-amber-800">{windowWidth}<span className="text-[35px]">px</span></p>
+      <p className="text-[90px] font-bold mt-4 text-gray-700 transition-all duration-300 hover:rotate-6 hover:scale-125 hover:text-amber-600  selection:text-amber-600 cursor-pointer">{windowWidth}<span className="text-[35px] text-amber-600">px</span></p>
     </div>
   );
 }
